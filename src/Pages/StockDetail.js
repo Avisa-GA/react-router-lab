4457583bd2bdacd49a1b6a7da5e84f2d2ac18ed4
Avisa-SEI <aap571420@gmail.com>
2021-06-26 08:58:56 -0700

    wip

--- src/Pages/StockDetail.js
+++ src/Pages/StockDetail.js
@@ -6,34 +6,7 @@ export default function StockDetail(props) {
 
     const symbol = props.match.params.symbol;
 
-     const name = data.map((ele) => {
-         if (ele.symbol === symbol) {
-         return ele.name
-         }
-     })
-     const lastPrice = data.map((ele) => {
-        if (ele.symbol === symbol) {
-        return ele.lastPrice
-        }
-    })
-
-    const high = data.map((ele) => {
-        if (ele.symbol === symbol) {
-        return ele.high
-        }
-    })
-
-    const low = data.map((ele) => {
-        if (ele.symbol === symbol) {
-        return ele.low
-        }
-    })
-
-    const open = data.map((ele) => {
-        if (ele.symbol === symbol) {
-        return ele.open
-        }
-    })
+    const stock = props.stocks.find(s => s.symbol === symbol)
  
     return (
         <div>
@@ -50,12 +23,12 @@ export default function StockDetail(props) {
    </thead>
    <tbody>
      <tr>
-       <td>{name}</td>
-       <td>{symbol}</td>
-       <td>{lastPrice}</td>
-       <td>{high}</td>
-       <td>{low}</td>
-       <td>{open}</td>
+       <td>{stock.name}</td>
+       <td>{stock.symbol}</td>
+       <td>{stock.lastPrice}</td>
+       <td>{stock.high}</td>
+       <td>{stock.low}</td>
+       <td>{stock.open}</td>
      </tr>
    
   </tbody>
